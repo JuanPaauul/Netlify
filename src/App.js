97c9092115ac7c7d7a99643f03c9ce170ea82bf7
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-08-27 09:47:28 -0400

    Last try :(

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 const nombre = document.querySelector("#nombre-input");
 const genero = document.querySelector("#genero-input");
 const edad = document.querySelector("#edad-input");
+const idioma = document.querySelector("#idioma-input");
 const form = document.querySelector("#saludador-form");
 
 form.addEventListener("submit", (event) => {
   var d = new Date();
   var h = d.getHours();
-  const hora = "buenos dias ";
-  const saludo = "Hola ";
-  const generoSaludo = "señor ";
-  if(edad.value>30){
+  var hora = "buenos dias ";
+  var saludo = "Hola ";
+  var generoSaludo = "señor ";
+  if(parseInt(edad.value)>30){
     if(genero.value == "Femenino")
       generoSaludo = "señora ";
     saludo = saludo + generoSaludo;
@@ -18,5 +19,5 @@ form.addEventListener("submit", (event) => {
     hora = "buenas tardes ";
   if(h>16)
     hora = "buenas noches ";
-  alert(saludo + hora + nombre.value + ":" + genero.value + h );
+  alert(saludo + hora + nombre.value);
 });
